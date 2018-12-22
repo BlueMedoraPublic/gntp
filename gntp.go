8be0dd34f2408019f347b0da3cfca704088259f8
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-12-22 14:52:23 -0500

    Create gntp.go

---
+++ gntp.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "github.com/beevik/ntp"
+)
+
+// returns the current hour, minutes, seconds
+// example: <hour> <min> <second> | 15 30 12
+func main() {
+    t, err := ntp.Time("pool.ntp.org")
+    if err != nil {
+        fmt.Printf(err.Error())
+    } else {
+        fmt.Println(strconv.Itoa(t.Hour()), strconv.Itoa(t.Minute()),  strconv.Itoa(t.Second()))
+    }
+}
